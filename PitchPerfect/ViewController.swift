f4193c635b52a0d65b8f81cc60bec149f8de7139
Cece Soudaly <ttoukta@yahoo.com>
2016-08-14 22:31:48 -0500

    adding some more buttons and logic to the project.

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordLabel: UILabel!
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //blah blah
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,7 +30,25 @@ class ViewController: UIViewController {
     @IBAction func recordAudio(sender: AnyObject) {
         print("Audio Record button has been pressed.")
         
+        recordLabel.text = "Recording in progress..."
+        recordButton.enabled=false
+        stopButton.enabled=true
+        
+        
     }
 
+    @IBAction func stopRecord(sender: AnyObject) {
+        print("Stop Record button has been pressed.")
+        recordLabel.text = "Stop Audio..."
+        stopButton.enabled=false
+        recordButton.enabled=true
+        recordLabel.text = "Tap to Record"
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+         stopButton.enabled=false
+    }
+    
 }
 
